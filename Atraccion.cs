6ae6487162e001342a87d4fc7714af7d73b57965
SebasOso <criptodesc09@gmail.com>
2021-10-13 18:08:53 -0500

    Update 13/10/21

--- Atraccion.cs
+++ Atraccion.cs
@@ -11,18 +11,19 @@ namespace ParqueDiversiones
         private int limite_edad;
         private float limite_estatura;
         private int costo;
-        private int cantidad_ingresos;
-        private List<Usuario> usuarios_recibidos;
+        private List<Entrada> entradas = new List<Entrada>();
+      
+       
 
-        public Atraccion(string codigo, string nombre, int limite_edad, float limite_estatura, int costo, int cantidad_ingresos, List<Usuario> usuarios_recibidos)
+        public Atraccion(string codigo, string nombre, int limite_edad, float limite_estatura, int costo, int cantidad_ingresos)
         {
             this.codigo = codigo;
             this.nombre = nombre;
             this.limite_edad = limite_edad;
             this.limite_estatura = limite_estatura;
             this.costo = costo;
-            this.cantidad_ingresos = cantidad_ingresos;
-            this.usuarios_recibidos = usuarios_recibidos;
+           
+           
         }
 
         public string Codigo { get => codigo; set => codigo = value; }
@@ -30,7 +31,7 @@ namespace ParqueDiversiones
         public int Limite_edad { get => limite_edad; set => limite_edad = value; }
         public float Limite_estatura { get => limite_estatura; set => limite_estatura = value; }
         public int Costo { get => costo; set => costo = value; }
-        public int Cantidad_ingresos { get => cantidad_ingresos; set => cantidad_ingresos = value; }
-        internal List<Usuario> Usuarios_recibidos { get => usuarios_recibidos; set => usuarios_recibidos = value; }
+      
+        
     }
 }
--- Manilla.cs
+++ Manilla.cs
@@ -6,13 +6,27 @@ namespace ParqueDiversiones
 {
     class Manilla
     {
+        private int edad;
+        private float estatura;
+        private string nombre_completo;
         private int saldo;
+       
 
-        public Manilla(int saldo)
+
+        public Manilla(int edad, float estatura, string nombre_completo,int saldo)
         {
+            this.edad = edad;
+            this.estatura = estatura;
+            this.nombre_completo = nombre_completo;
             this.saldo = saldo;
+          
         }
 
-        public int Saldo { get => saldo; set => saldo = value; }
+        public int Edad { get => edad; set => edad = value; }
+        public float Estatura { get => estatura; set => estatura = value; }
+        public string Nombre_completo { get => nombre_completo; set => nombre_completo = value; }
+        public int Saldo {get => saldo; set => saldo = value;}
+        
+       
     }
 }
